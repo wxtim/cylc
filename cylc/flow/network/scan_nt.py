181eeb2311e1c5c4c8ea595a1455b09e398ea3f8
Oliver Sanders <oliver.sanders@metoffice.gov.uk>
2020-06-17 16:07:51 +0100

    async ++

--- cylc/flow/network/scan_nt.py
+++ cylc/flow/network/scan_nt.py
@@ -14,10 +14,15 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import asyncio
 from pathlib import Path
-import re
 
 from cylc.flow import LOG
+from cylc.flow.async_util import (
+    Pipe,
+    asyncqgen,
+    scandir
+)
 from cylc.flow.cfgspec.glbl_cfg import glbl_cfg
 from cylc.flow.network.client import (
     SuiteRuntimeClient, ClientError, ClientTimeout)
@@ -31,120 +36,6 @@ SERVICE = Path(SuiteFiles.Service.DIRNAME)
 CONTACT = Path(SuiteFiles.Service.CONTACT)
 
 
-class Pipe:
-    """An asynchronous pipe implementation in pure Python.
-
-    Example:
-        A generator to begin our pipe with:
-        >>> @Pipe
-        ... async def arange():
-        ...    for i in range(10):
-        ...        yield i
-
-        A filter which returns a boolean:
-        >>> @Pipe
-        ... async def even(x):
-        ...    return x % 2 == 0
-
-        A transformation returns anything other than a boolean:
-        >>> @Pipe
-        ... async def mult(x, y):
-        ...    return x * y
-
-        Assemble them into a pipe
-        >>> mypipe = arange | even | mult(2)
-        >>> print(mypipe)
-        arange()
-        >>> repr(mypipe)
-        'arange() | even() | mult(2)'
-
-        Write a function to "consume items":
-        >>> async def consumer(pipe):
-        ...     async for item in pipe:
-        ...         print(item)
-
-        Run pipe run:
-        >>> import asyncio
-        >>> asyncio.run(consumer(mypipe))
-        0
-        4
-        8
-        12
-        16
-
-        Real world examples will involve a bit of awaiting.
-
-    """
-
-    def __init__(self, func):
-        self.func = func
-        self.args = tuple()
-        self.kwargs = {}
-        self._left = None
-        self._right = None
-
-    def __call__(self, *args, **kwargs):
-        self.args = args
-        self.kwargs = kwargs
-        return self
-
-    async def __aiter__(self):
-        coros = self.__iter__()
-        gen = next(coros)
-        coros = list(coros)
-        async for item in gen.func():
-            for coro in coros:
-                ret = await coro.func(item, *coro.args, **coro.kwargs)
-                if ret is False:
-                    break
-                if ret is True:
-                    pass
-                else:
-                    item = ret
-            else:
-                yield item
-
-    def __or__(self, other):
-        other._left = self
-        self.fastforward()._right = other
-        # because we return self we only need __or__ not __ror__
-        return self
-
-    def rewind(self):
-        """Return the head of the pipe."""
-        ptr = self
-        while ptr._left:
-            ptr = ptr._left
-        return ptr
-
-    def fastforward(self):
-        """Return the tail of the pipe."""
-        ptr = self
-        while ptr._right:
-            ptr = ptr._right
-        return ptr
-
-    def __iter__(self):
-        ptr = self.rewind()
-        while ptr._right:
-            yield ptr
-            ptr = ptr._right
-        yield ptr
-
-    def __repr__(self):
-        return ' | '.join((str(ptr) for ptr in self))
-
-    def __str__(self):
-        args = ''
-        if self.args:
-            args = ', '.join(map(repr, self.args))
-        if self.kwargs:
-            if args:
-                args += ', '
-            args += ', '.join(f'{k}={repr(v)}' for k, v in self.kwargs.items())
-        return f'{self.func.__name__}({args})'
-
-
 def regex_combine(patterns):
     """Join regex patterns.
 
@@ -156,10 +47,15 @@ def regex_combine(patterns):
     return rf'({"|".join(patterns)})'
 
 
-def dir_is_flow(path):
+async def dir_is_flow(listing):
     """Return True if a Path contains a flow at the top level."""
+    listing = [
+        path.name
+        for path in listing
+    ]
     return (
-        (path / SERVICE).exists()
+        SERVICE in listing
+        or 'suite.rc' in listing
     )
 
 
@@ -169,26 +65,25 @@ async def scan(run_dir=None):
         run_dir = Path(
             glbl_cfg().get_host_item('run directory').replace('$HOME', '~')
         ).expanduser()
-    stack = [
-        subdir
-        for subdir in run_dir.iterdir()
-        if subdir.is_dir()
-    ]
-    for path in stack:
-        name = str(path.relative_to(run_dir))
-        if dir_is_flow(path):
+    stack = asyncio.Queue()
+    for subdir in await scandir(run_dir):
+        if subdir.is_dir():
+            await stack.put(subdir)
+
+    # for path in stack:
+    async for path in asyncqgen(stack):
+        contents = await scandir(path)
+        if await dir_is_flow(contents):
             # this is a flow directory
             yield {
-                'name': name,
+                'name': str(path.relative_to(run_dir)),
                 'path': path,
             }
         else:
             # we may have a nested flow, lets see...
-            stack.extend([
-                subdir
-                for subdir in path.iterdir()
-                if subdir.is_dir()
-            ])
+            for subdir in contents:
+                if subdir.is_dir():
+                    await stack.put(subdir)
 
 
 @Pipe
