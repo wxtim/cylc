7d7fd9de1332ed4f0b3e637fe0a2cb75fbca1182
Ronnie Dutta <61982285+MetRonnie@users.noreply.github.com>
2024-06-27 15:05:39 +0100

    Schema: use `Enum` for task run mode instead of `String`

--- cylc/flow/network/schema.py
+++ cylc/flow/network/schema.py
@@ -67,6 +67,7 @@ from cylc.flow.util import sstrip
 from cylc.flow.workflow_status import StopMode
 
 if TYPE_CHECKING:
+    from enum import Enum
     from graphql import ResolveInfo
     from graphql.type.definition import (
         GraphQLNamedType,
@@ -596,6 +597,46 @@ class TimeZone(ObjectType):
     string_extended = String()
 
 
+def describe_run_mode(run_mode: Optional['Enum']) -> str:
+    """Returns description for a workflow/task run mode."""
+    if not run_mode:
+        return ""
+    mode = run_mode.value
+    if mode == RunMode.WORKFLOW:
+        return "Default to the workflow's run mode."
+    if mode == RunMode.LIVE:
+        return "Tasks will run normally."
+    if mode == RunMode.SIMULATION:
+        return (
+            "Simulates job submission with configurable execution time "
+            "(does not submit real jobs)."
+        )
+    if mode == RunMode.DUMMY:
+        return "Submits real jobs with empty scripts."
+    if mode == RunMode.SKIP:
+        return (
+            "Skips job submission; sets required outputs (by default) or "
+            "configured outputs."
+        )
+    return ""
+
+
+WorkflowRunMode = graphene.Enum(
+    'WorkflowRunMode',
+    [(m.capitalize(), m) for m in RunMode.WORKFLOW_MODES],
+    description=describe_run_mode,
+)
+"""The run mode for the workflow."""
+
+
+TaskRunMode = graphene.Enum(
+    'TaskRunMode',
+    [(m.capitalize(), m) for m in (RunMode.WORKFLOW, *RunMode.WORKFLOW_MODES)],
+    description=describe_run_mode,
+)
+"""The run mode for tasks."""
+
+
 class Workflow(ObjectType):
     class Meta:
         description = """Global workflow info."""
@@ -822,7 +863,7 @@ class Runtime(ObjectType):
     directives = graphene.List(RuntimeSetting, resolver=resolve_json_dump)
     environment = graphene.List(RuntimeSetting, resolver=resolve_json_dump)
     outputs = graphene.List(RuntimeSetting, resolver=resolve_json_dump)
-    run_mode = graphene.String(default_value=RunMode.WORKFLOW)
+    run_mode = TaskRunMode(default_value=TaskRunMode.Workflow.name)
 
 
 RUNTIME_FIELD_TO_CFG_MAP = {
